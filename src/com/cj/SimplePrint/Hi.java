0335698463d34da9b094545ce6d8a3fd7ab6a7f5
Nil7997 <nileshm7997@gmail.com>
2025-08-02 15:48:35 +0530

    first commit

---
+++ src/com/cj/SimplePrint/Hi.java
@@ -0,0 +1,18 @@
+package com.cj.SimplePrint;
+
+public class Hi
+{
+	public void HelloEveryOne() 
+	{
+		System.out.println("Hello Every One ........!");		
+	}
+	public void GoodMorning() 
+	{
+		System.out.println("Good Morning ........!");		
+	}
+	public void HelloWord() 
+	{
+		System.out.println("Hello World........!");		
+	}
+
+}
---
+++ src/com/cj/main/Test.java
@@ -0,0 +1,20 @@
+package com.cj.main;
+
+import com.cj.SimplePrint.Hi;
+
+public class Test {
+
+	// simple Way Method Calling And normally Prints Methods....
+	public static void main(String[] args)
+	{
+		Hi h=new Hi();
+		h.HelloEveryOne();
+		System.out.println(".............");
+		h.GoodMorning();
+		System.out.println(".............");
+		h.HelloWord();
+		System.out.println(".............");
+		
+	}
+
+}
